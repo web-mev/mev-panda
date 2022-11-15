b337834c73dd305735d589e3e0ce05d4c6877ec4
Brian Lawney <blawney@hsph.harvard.edu>
2022-11-15 09:43:50 -0500

    Increasing size of allowable input matrix from 15k to 100k

--- docker/panda.py
+++ docker/panda.py
@@ -8,7 +8,7 @@ DEFAULT_MOTIF_FILE = '/opt/software/resources/tissues_motif.tsv'
 DEFAULT_PPI_FILE = '/opt/software/resources/tissues_ppi.tsv'
 
 # maximum number of rows to keep
-NMAX = 15000
+NMAX = 100000
 
 def handle_dummy_args(args):
     '''
